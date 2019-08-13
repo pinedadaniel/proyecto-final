2eb0707ebefa29663896e01231128a0c329483ec
yeison1998 <jesusstivensuarez@gmail.com>
2019-08-13 13:35:49 -0500

    Make

--- src/pages/HomePages.js
+++ src/pages/HomePages.js
@@ -1,15 +1,12 @@
 import React from 'react';
 import './styles/HomeStyle.css';
-import Carousel from '../components/carouselComponent';
-import Make from '../components/makeComponent';
 
 
 class Home extends React.Component {
     render() {
         return (
             <div className="Home">
-                <Carousel />
-                <Make/>
+               
 
             </div>
         );
